165567c2ceab4533df04b235cbb4565c6cfa1f55
Yana Soroka <cernyyana@gmail.com>
2025-11-13 20:11:13 +0300

    Add class FormsValidation

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-class FormValidation{
+class FormsValidation{
     selectors = {
         form: '[data-js-form]',
         fieldErrors: '[data-js-form-field-errors]'
@@ -10,103 +10,93 @@ class FormValidation{
         tooShort: ({minLength}) => `Слишком короткое сообщение, минимум символов ${minLength}`,
     }
 
-    constructor(){
-        this.bindEvents() //обработчик событий
+    constructor() {
+    this.bindEvents()
+  }
+
+  manageErrors(fieldControlElement, errorMessages) {
+    const fieldErrorsElement = fieldControlElement.parentElement.querySelector(this.selectors.fieldErrors)
+
+    fieldErrorsElement.innerHTML = errorMessages
+      .map((message) => `<span class="field__error">${message}</span>`)
+      .join('')
+  }
+
+  validateField(fieldControlElement) {
+    const errors = fieldControlElement.validity
+    const errorMessages = []
+
+    Object.entries(this.errorMessages).forEach(([errorType, getErrorMessage]) => {
+      if (errors[errorType]) {
+        errorMessages.push(getErrorMessage(fieldControlElement))
+      }
+    })
+
+
+    this.manageErrors(fieldControlElement, errorMessages)
+
+    const isValid = errorMessages.length === 0
+
+    fieldControlElement.ariaInvalid = !isValid
+
+    return isValid
+  }
+
+  onBlur(event) {
+    const { target } = event
+    const isFormField = target.closest(this.selectors.form)
+    const isRequired = target.required
+
+    if (isFormField && isRequired) {
+      this.validateField(target)
     }
-//собирает ошибки для заданного поля и управляет отображением ошибок
-    manageErrors(fieldControlElement, errorMessages){
-        const fieldErrorsElement = fieldControlElement.parentElement.queriSelector(this.selectors.fieldErrors)
-        
-        fieldErrorsElement.innerHTML = errorMessages
-        .map((message) => `<span class"field__error">${message}</span>`)
-        .join('')
+  }
+
+  onChange(event) {
+    const { target } = event
+    const isRequired = target.required
+    const isToggleType = ['radio', 'checkbox'].includes(target.type)
+
+    if (isToggleType && isRequired) {
+      this.validateField(target)
+    }
+  }
+
+  onSubmit(event) {
+    const isFormElement = event.target.matches(this.selectors.form)
+    if (!isFormElement) {
+      return
     }
 
-    validatteField(fieldControlElement){
-        const errors = fieldControlElement.validity
-        const errorMessages =[]
+    const requiredControlElements = [...event.target.elements].filter(({ required }) => required)
+    let isFormValid = true
+    let firstInvalidFieldControl = null
 
-        Object.entries(this.errorMessages).forEach(([errorType, getErrorMassage]) => {
-            if (errors[errorType]){
-                errorMessages.push(getErrorMassage(fieldControlElement))
-            }
-        })
+    requiredControlElements.forEach((element) => {
+      const isFieldValid = this.validateField(element)
 
-        this.manageErrors(fieldControlElement, errorMessages)
-            const isValid = errorMessages.length === 0
-            fieldControlElement.ariaInvalid = !isValid
+      if (!isFieldValid) {
+        isFormValid = false
 
-            return isValid
-
-    }
-
-    onChange(event){
-        const {target} = event
-        const isRequired = target.required 
-        const isToggleType = ['radio', 'checkbox'].includes(target.type)
-
-        if (isToggleType && isRequired){
-            this.validatteField(target)
+        if (!firstInvalidFieldControl) {
+          firstInvalidFieldControl = element
         }
+      }
+    })
 
-        
+    if (!isFormValid) {
+      event.preventDefault()
+      firstInvalidFieldControl.focus()
     }
+  }
 
-    onBlur(event){
-        const {target} = event
-        const isFormField = target.closest(this.selectors.form)
-        const isRequired = target.required
-
-        if (isFormField && isRequired){
-            this.validatteField(target)
-        }
-
-    }
-
-    onSubmit(event){
-        const isFormElement = event.target.matches(this.selectors.form)
-        if(!isFormElement){
-            return
-        }
-
-        const requiredControlElements = [...event.target.elements].filter(({required}) => required)
-
-        let isFormValid = true
-        let firstItvalidFieldControl = null
-
-        requiredControlElements.forEach((element) => {
-            const isFieldValid = this.validatteField(element)
-
-            if(!isFieldValid){
-                isFormValid = false
-
-                if(!firstItvalidFieldControl){
-                    firstItvalidFieldControl = element
-                }
-            }
-
-        })
-
-        if(!isFormValid){
-            event.prevntDefault()
-            firstItvalidFieldControl.focus()
-        }
-    }
-
-
-bindEvents(){
+  bindEvents() {
     document.addEventListener('blur', (event) => {
-        this.onBlur(event)
-    },{capture: true}
-)
-document.addEventListener('change', (event) => this.onChange(event))
-document.addEventListener('submit', (event) => this.onSubmit(event))
-}
-    
-
-
-
+      this.onBlur(event)
+    }, { capture: true })
+    document.addEventListener('change', (event) => this.onChange(event))
+    document.addEventListener('submit', (event) => this.onSubmit(event))
+  }
 }
 
-
-new FormValidation()
+new FormsValidation()
